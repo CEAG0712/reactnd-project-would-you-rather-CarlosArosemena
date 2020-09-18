73c863f4002de29eb21c07b37f000746b031695f
AgileIntelligence <io.agileintelligence@gmail.com>
2020-09-18 05:49:30 -0400

    type: feature body: implemented sample view and initialized data

---
+++ src/components/ui-elements/MainGrid.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+
+export const MainGrid = ({ children }) => (
+  <Grid padded="vertically" columns={1} centered>
+    <Grid.Row>
+      <Grid.Column style={{ maxWidth: 550 }}>{children}</Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
