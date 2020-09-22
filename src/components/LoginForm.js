5a3742c7b94e861de90cbc875d991737598dbe56
AgileIntelligence <io.agileintelligence@gmail.com>
2020-09-22 06:02:38 -0400

    type: feature body: login form sets the authed user on the state

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,23 +1,42 @@
 import React, { Component } from "react";
 import { Form, Header } from "semantic-ui-react";
-
-const optionTest = [
-  {
-    key: 1,
-    text: "sample1",
-    value: "sample1",
-  },
-  {
-    key: 2,
-    text: "sample2",
-    value: "sample2",
-  },
-];
+import { connect } from "react-redux";
+import { setAuthUser } from "../actions/authUser";
 
 class LoginForm extends Component {
+  state = {
+    value: "",
+  };
+
+  onChange = (e, { value }) => {
+    this.setState({ value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { setAuthUser } = this.props;
+    const authUser = this.state.value;
+    setAuthUser(authUser);
+  };
+
+  optionsDropDown = () => {
+    const { users } = this.props;
+
+    return users.map((user) => ({
+      key: user.id,
+      text: user.name,
+      value: user.id,
+      image: { avatar: true, src: user.avatarURL },
+    }));
+  };
+
   render() {
+    const { value } = this.state;
+    const disabled = value === "" ? true : false;
+
     return (
-      <Form>
+      <Form onSubmit={this.onSubmit}>
         <Header as="h2" color="blue">
           Sign In
         </Header>
@@ -26,11 +45,20 @@ class LoginForm extends Component {
           fluid
           selection
           scrolling
-          options={optionTest}
+          options={this.optionsDropDown()}
+          value={value}
+          onChange={this.onChange}
         />
+        <Form.Button content="Login" positive disabled={disabled} fluid />
       </Form>
     );
   }
 }
 
-export default LoginForm;
+function mapStateToProps({ users }) {
+  return {
+    users: Object.values(users),
+  };
+}
+
+export default connect(mapStateToProps, { setAuthUser })(LoginForm);
