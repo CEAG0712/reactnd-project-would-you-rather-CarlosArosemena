687e5c2336dfe5486913f73e7be061394311ae2b
AgileIntelligence <io.agileintelligence@gmail.com>
2020-09-25 05:17:55 -0400

    type:feature body: implemented nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import { MainGrid } from "./components/ui-elements/MainGrid";
 import { handleInitialData } from "./actions/shared";
 import { connect } from "react-redux";
 import Login from "./components/Login";
+import NavBar from "./components/ui-elements/NavBar";
 
 class App extends Component {
   componentDidMount() {
@@ -11,14 +13,33 @@ class App extends Component {
   }
 
   render() {
+    const { authUser } = this.props;
     return (
-      <div className="App">
-        <MainGrid>
-          <Login />
-        </MainGrid>
-      </div>
+      <Router>
+        <div className="App">
+          {authUser === null ? (
+            <Route
+              render={() => (
+                <MainGrid>
+                  <Login />
+                </MainGrid>
+              )}
+            />
+          ) : (
+            <Fragment>
+              <NavBar />
+            </Fragment>
+          )}
+        </div>
+      </Router>
     );
   }
 }
 
-export default connect(null, { handleInitialData })(App);
+const mapStateToProps = ({ authUser }) => {
+  return {
+    authUser,
+  };
+};
+
+export default connect(mapStateToProps, { handleInitialData })(App);
